d7497929fb290ad5fc6029b32f99587966f9ca67
imeshcheryakova <imeshcher@chicagobooth.edu>
2015-07-17 20:56:47 -0500

    complete_weather

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -17,17 +17,45 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    url="http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+    open(url)
+    raw_data=open(url).read
+    parsed_data=JSON.parse(raw_data)
+    results=parsed_data["results"]
+    first=results[0]
+    geometry=first["geometry"]
+    location=geometry["location"]
+    location["lat"]
+    location["lng"]
+
+    url="https://api.forecast.io/forecast/b0e7cfcc7dba2a9ad8e8fa9d6267b500/#{location["lat"]},#{location["lng"]}"
+
+    open(url)
+    raw_data=open(url).read
+    parsed_data=JSON.parse(raw_data)
+    results=parsed_data["currently"]
+    temperature=results["temperature"]
+    summary=results["summary"]
+
+    results2=parsed_data["minutely"]
+    summary1=results2["summary"]
+
+    results3=parsed_data["hourly"]
+    summary2=results3["summary"]
+
+    results4=parsed_data["daily"]
+    summary3=results4["summary"]
 
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = temperature
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = summary
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = summary1
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = summary2
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = summary3
 
     render("street_to_weather.html.erb")
   end
